fa0eb8c3d337a3b87907ded4edc1926a5c109b75
Nicolas De Loof <nicolas.deloof@gmail.com>
2019-04-24 11:16:23 +0200

    use official docker client library
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- gc.go
+++ gc.go
@@ -16,24 +16,27 @@
 package main
 
 import (
-	"github.com/fsouza/go-dockerclient"
-	log "github.com/Sirupsen/logrus"
-	"time"
+	"context"
 	"flag"
-	"os"
-	"strings"
-	"path"
 	"github.com/boltdb/bolt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"path"
+	"strings"
+	"time"
 )
 
 var (
-	client *docker.Client
-	db *bolt.DB
-	dbPath = flag.String("db", "/var/db/docker-gc/state.db", "Location of the database file")
-	debug = flag.Bool("debug", false, "Enable debug output")
-	maxAge = flag.Duration("maxAge", 72 * time.Hour, "max duration for an unused image")
-	lastUse = map[string]time.Time{}
-	purgeFrequency = flag.Duration("purgeFrequency", 57 * time.Second, "How often the image purge will be run")
+	cli            *client.Client
+	db             *bolt.DB
+	dbPath         = flag.String("db", "/var/db/docker-gc/state.db", "Location of the database file")
+	debug          = flag.Bool("debug", false, "Enable debug output")
+	maxAge         = flag.Duration("maxAge", 72*time.Hour, "max duration for an unused image")
+	lastUse        = map[string]time.Time{}
+	purgeFrequency = flag.Duration("purgeFrequency", 57*time.Second, "How often the image purge will be run")
 )
 
 const (
@@ -41,16 +44,16 @@ const (
 )
 
 func init() {
-	c, err := docker.NewClientFromEnv()
+	c, err := client.NewEnvClient()
 	if err != nil {
 		log.Fatal("Failed to setup docker client " + err.Error())
 	}
-	client = c
+	cli = c
 }
 
 func removeImage(id string) {
 	log.WithField("id", id).Info("Removing image")
-	err := client.RemoveImage(id)
+	_, err := cli.ImageRemove(context.Background(), id, types.ImageRemoveOptions{})
 	if err != nil {
 		log.WithError(err).WithField("id", id).Error("Cannot remove image")
 		return
@@ -74,18 +77,18 @@ func updateImageLastUsage(id string, usage time.Time) {
 	if db != nil {
 		db.Update(func(tx *bolt.Tx) error {
 			log.WithFields(log.Fields{"id": id, "usage": usage}).
-			Debug("Updating database")
+				Debug("Updating database")
 			b := tx.Bucket([]byte(BUCKET_IMAGE))
 			encoded, err := usage.GobEncode()
 			if err != nil {
 				log.WithError(err).WithFields(log.Fields{"id": id, "usage": usage}).
-				Error("Cannot update image data")
+					Error("Cannot update image data")
 				return err
 			}
 			err = b.Put([]byte(id), encoded)
 			if err != nil {
 				log.WithError(err).WithFields(log.Fields{"id": id, "usage": usage}).
-				Error("Cannot update image data")
+					Error("Cannot update image data")
 				return err
 			}
 			return nil
@@ -99,28 +102,33 @@ func updateImageLastUsage(id string, usage time.Time) {
 func loadImageDataFromDocker() {
 	now := time.Now()
 	log.Info("Setting last use from containers")
-	containers, err := client.ListContainers(docker.ListContainersOptions{All: true})
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
 	if err != nil {
 		log.WithError(err).Warn("Cannot get list of containers, image last usage may be less accurate")
 	} else {
 		for _, container := range containers {
 			log.WithField("Container", container.ID).Debug("Reading container")
-			img, err := client.InspectImage(container.Image)
+			img, _, err := cli.ImageInspectWithRaw(context.Background(), container.Image)
 			if err != nil {
 				log.WithError(err).WithField("Container", container.ID).
-				Warn("Cannot inspect image for container")
+					Warn("Cannot inspect image for container")
 				continue
 			}
 			var usage time.Time
 			if strings.HasPrefix(container.Status, "Exit") {
 				log.WithField("Container", container.ID).Debug("Container exited, adjusting image last usage")
-				details, err := client.InspectContainer(container.ID)
+				details, err := cli.ContainerInspect(context.Background(), container.ID)
 				if err != nil {
 					log.WithField("Container", container.ID).
-					WithError(err).Warn("Cannot inspect container, skipping image update")
+						WithError(err).Warn("Cannot inspect container, skipping image update")
 					continue
 				}
-				usage = details.State.FinishedAt
+				usage, err = time.Parse(time.RFC3339, details.State.FinishedAt)
+				if err != nil {
+					log.WithError(err).WithField("Container", container.ID).
+						Warn("Cannot parse FinishedAt for container")
+					continue
+				}
 
 			} else {
 				usage = now
@@ -132,7 +140,7 @@ func loadImageDataFromDocker() {
 	}
 
 	log.Info("Reading image data from Docker")
-	images, err := client.ListImages(docker.ListImagesOptions{})
+	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
 	if err != nil {
 		log.WithError(err).Warn("Cannot list images from Docker")
 		return
@@ -183,7 +191,7 @@ func initDatabase() error {
 				log.WithError(err).WithField("Image", id).Warn("Cannot decode last usage")
 			}
 			log.WithFields(log.Fields{
-				"Image": string(id),
+				"Image":    string(id),
 				"Last use": decoded,
 			}).Debug("Retrieved image data")
 			lastUse[string(id)] = decoded
@@ -229,7 +237,9 @@ func main() {
 }
 
 func collect() {
-	dangling, err := client.ListImages(docker.ListImagesOptions{Filter: "dangling=true" })
+	filters := filters.NewArgs()
+	filters.Add("dangling", "true")
+	dangling, err := cli.ImageList(context.Background(), types.ImageListOptions{Filters: filters})
 	if err != nil {
 		// TODO isn't Fatal a be too much
 		log.WithError(err).Fatal("Cannot get list of dangling images")
@@ -240,16 +250,16 @@ func collect() {
 	}
 
 	inUse := map[string]bool{}
-	containers, err := client.ListContainers(docker.ListContainersOptions{All:true})
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
 	if err != nil {
 		// TODO isn't Fatal a be too much
 		log.WithError(err).Fatal("Cannot get list of containers")
 	}
 	for _, container := range containers {
-		img, err := client.InspectImage(container.Image)
+		img, _, err := cli.ImageInspectWithRaw(context.Background(), container.Image)
 		if err != nil {
 			log.WithError(err).WithField("Container", container.ID).
-			Warn("Cannot inspect image for container")
+				Warn("Cannot inspect image for container")
 			continue
 		}
 		log.WithFields(log.Fields{"image": img.ID, "container": container.ID}).Debug("Image is used by container")
@@ -258,14 +268,14 @@ func collect() {
 
 	max := time.Now().Add(time.Duration(-1 * maxAge.Nanoseconds()))
 	log.WithField("Since", max.Truncate(time.Second)).Debug("Purging all unused image")
-	images, err := client.ListImages(docker.ListImagesOptions{})
+	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, image := range images {
 		id := image.ID
 		if use, ok := lastUse[id]; ok && use.Before(max) && !inUse[id] {
-			log.WithFields(log.Fields{"id":id, "use": use}).Info("Purging unused image")
+			log.WithFields(log.Fields{"id": id, "use": use}).Info("Purging unused image")
 			removeImage(image.ID)
 		}
 	}
